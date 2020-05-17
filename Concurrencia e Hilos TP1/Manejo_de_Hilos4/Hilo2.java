67f2a31c74f85160ff5bad6d4e0ca40ee1d4469e
fsosa <fsosa1>
2020-05-17 02:49:30 -0300

    sincronizacion

---
+++ Concurrencia e Hilos TP1/Manejo_de_Hilos4/Hilo2.java
@@ -0,0 +1,21 @@
+public class Hilo2 extends Thread{
+
+
+    @Override
+    public void run() {
+        for( int i=0; i<= 5; i++) {
+            System.out.print("E");
+
+
+            try{
+
+                Hilo1.sleep(1000);
+            }catch (InterruptedException e){
+                System.out.println("Error en la clase Hilo 2 " +e );
+            }
+
+        }
+
+    }
+    
+}
